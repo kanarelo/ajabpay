abcdd45c505a23d4542b7d87c05977f19ad36738
Mukewa Wekalao <kanarelo@gmail.com>
2016-09-22 17:17:26 +0300

    Paypal items now gotten from BaseConfig

--- src/ajabpay/app/core/transactions/paypal/utils.py
+++ src/ajabpay/app/core/transactions/paypal/utils.py
@@ -1,17 +1,15 @@
 import paypalrestsdk
 from decimal import Decimal as D
 
-PAYPAL_MODE = "sandbox"
-PAYPAL_CLIENT_ID = "ATo_Io1R9XCX9SmfHdGbeXYSKZnireDROhLUwcjO_VtLiUx7yB7CuMjTWJO0JgfGSXhxCLsLXna3KIn0"
-PAYPAL_CLIENT_SECRET = "EIbbidsOH9Y_2aXPiInRs7Wf-2Emn6fBzTfHXjxgZwC23Lu00zhvA2rImcz-7nkr1OfaDNuwq4yUWgYV"
+from ajabpay.config import BaseConfig
 
 round_down = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_DOWN')
 round_up = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_UP')
 format_amount = lambda amount: format(round_down(amount), '.2f')
 
 def create_paypalrestsdk_api():
-    return paypalrestsdk.Api({
-        "mode": PAYPAL_MODE,
-        "client_id": PAYPAL_CLIENT_ID,
-        "client_secret": PAYPAL_CLIENT_SECRET 
-    })
+    return paypalrestsdk.Api(dict(
+        mode=BaseConfig.PAYPAL_MODE,
+        client_id=BaseConfig.PAYPAL_CLIENT_ID,
+        client_secret=BaseConfig.PAYPAL_CLIENT_SECRET
+    ))
