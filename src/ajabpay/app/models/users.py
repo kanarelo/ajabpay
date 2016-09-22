c386200389ee36c3a5f337f872f2a5e1e40e4a5b
Mukewa Wekalao <kanarelo@gmail.com>
2016-09-22 17:15:27 +0300

    updates users model

--- src/ajabpay/app/models/users.py
+++ src/ajabpay/app/models/users.py
@@ -5,26 +5,34 @@ class User(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer(), primary_key=True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    phone = db.Column(db.String(25))
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(25), unique=True, nullable=False)
 
-    date_joined = db.Column(db.DateTime())
-    last_login = db.Column(db.DateTime())
+    date_joined = db.Column(db.DateTime(), nullable=False)
+    last_login = db.Column(db.DateTime(), nullable=True)
 
-    def __init__(self, first_name=None, last_name=None, email=None, password=None, phone=None):
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+
+    date_updated = db.Column(db.DateTime(), nullable=False)
+    date_created = db.Column(db.DateTime(), nullable=False)
+
+    def __init__(self, first_name=None, last_name=None, email=None, password=None, phone=None, active=False):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.active = True
         self.phone = phone
+        self.active = active
 
         if password:
             self.password = User.hashed_password(password)
 
     def __unicode__(self):
+        return self.get_full_name()
+
+    def get_full_name(self):
         return '%s %s' % (self.first_name, self.last_name)
 
     @staticmethod
