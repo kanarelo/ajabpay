2a478a24cca9405dd2c70b806630b43ab346406c
Mukewa Wekalao <kanarelo@gmail.com>
2016-10-04 20:10:45 +0300

    sets up sentry

--- src/ajabpay/index.py
+++ src/ajabpay/index.py
@@ -16,13 +16,14 @@ Config = get_default_config()
 app = Flask(__name__, static_folder="../static", template_folder="./templates")
 app.config.from_object(Config)
 
-sentry = Sentry(app, dsn=Config.RAVEN_DSN)
 # ---setup logging
 if (Config.DEBUG == False) or ('production' in Config.STAGE):
     from pythonjsonlogger import jsonlogger
     formatter = jsonlogger.JsonFormatter()
+    sentry = Sentry(app, dsn=Config.RAVEN_DSN, logging=True, level=logging.INFO)
 else:
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    sentry = Sentry(app, dsn=Config.RAVEN_DSN, logging=True, level=logging.DEBUG)
 
 log_handler = RotatingFileHandler(Config.LOGGING_LOCATION, maxBytes=10000, backupCount=10)
 log_handler.setFormatter(formatter)
