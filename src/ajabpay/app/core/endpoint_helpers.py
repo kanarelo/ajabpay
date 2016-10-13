c9bb9b0056249132c1dea0427729cf551bcc9bd5
Mukewa Wekalao <kanarelo@gmail.com>
2016-10-13 13:41:39 +0300

    adds error_no, fixes small bugs

--- src/ajabpay/app/core/endpoint_helpers.py
+++ src/ajabpay/app/core/endpoint_helpers.py
@@ -2,13 +2,13 @@ from flask import render_template, jsonify, url_for
 from ajabpay.index import app, db
 
 @app.errorhandler(404)
-def page_not_found(e=None):
-    return render_template('404.html'), 404
+def page_not_found(e=None, error_no=None):
+    return render_template('404.html', error_no=error_no), 404
 
 @app.errorhandler(403)
-def access_forbidden(e=None):
-    return render_template('403.html'), 403
+def access_forbidden(e=None, error_no=None):
+    return render_template('403.html', error_no=error_no), 403
 
 @app.errorhandler(500)
-def internal_server_error(e=None):
-    return render_template('500.html'), 500
+def internal_server_error(e=None, error_no=None):
+    return render_template('500.html', error_no=error_no), 500
