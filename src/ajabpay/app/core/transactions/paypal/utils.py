2a2cd723a6163b773714e2ea1471ada3001c6769
Mukewa Wekalao <kanarelo@gmail.com>
2016-10-17 13:34:32 +0300

    imports round_up and round_down for export

--- src/ajabpay/app/core/transactions/paypal/utils.py
+++ src/ajabpay/app/core/transactions/paypal/utils.py
@@ -1,7 +1,5 @@
 from decimal import Decimal as D
 
 from ajabpay.index import app
-
-round_down = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_DOWN')
-round_up = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_UP')
-format_amount = lambda amount: format(round_down(amount), '.2f')
+from ajabpay.app.core.utils import (
+    round_up, round_down, format_amount)
