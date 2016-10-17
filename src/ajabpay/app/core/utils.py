4865bcad196fcb3679014de8247df519fd8fade1
Mukewa Wekalao <kanarelo@gmail.com>
2016-10-17 13:33:56 +0300

    Adds round_down and round_up

--- src/ajabpay/app/core/utils.py
+++ src/ajabpay/app/core/utils.py
@@ -3,6 +3,15 @@ import re
 import string
 import random
 
+from decimal import Decimal as D
+
+# round_down = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_DOWN')
+# round_up = lambda amount: D(str(amount)).quantize(D('.01'), rounding='ROUND_UP')
+format_amount = lambda amount: format(round_down(amount), '.2f')
+
+round_up = lambda (amount): (D(str(amount))).to_integral_value('ROUND_HALF_UP')
+round_down = lambda (amount) : (D(str(amount))).to_integral_value('ROUND_DOWN')
+
 def extract_string(regex, test_string):
     try:
         return re.search(regex, test_string).groups()[0]
@@ -21,7 +30,7 @@ def get_reference_no(limit=10,
     return ''.join(random.choice(possible_chars) for i in range(limit))
 
 def get_account_no(limit=8):
-    return get_reference_no(possible_chars="1234567890")
+    return get_reference_no(limit=limit, possible_chars="1234567890")
 
 SAFARICOM_PHONE_REGEX = r"^(\+2547|07)(?P<prefix>(([0-2]|[9])[0-9]))(?P<postfix>\d{6})$"
 VALID_SAFARICOM_NO_REGEX = SAFARICOM_PHONE_REGEX 
