58193fffb56211c043c6a92cc9771af3ee24262c
Mukewa Wekalao <kanarelo@gmail.com>
2016-10-17 13:33:35 +0300

    Adds fixes for P2M calculator

--- src/ajabpay/app/models/paypal.py
+++ src/ajabpay/app/models/paypal.py
@@ -1,4 +1,5 @@
 from ajabpay.index import db
+from ajabpay.app.core.utils import (round_down, round_up)
 from decimal import Decimal as D
 
 class PaypalProfile(db.Model):
@@ -51,84 +52,150 @@ class PaypalAddress(db.Model):
 
     date_created = db.Column(db.DateTime(), nullable=False)
 
-round_up = lambda (amount): ((amount * D('100')).to_integral_value()) / D('100')
-round_down = lambda (amount) : ((amount * D('100')).to_integral_value()) / D('100')
-
 class ConfigPaypalParameter(db.Model):
     __tablename__ = "configpaypalparameter"
     
     id = db.Column(db.Integer(), primary_key=True)
 
-    paypal_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #3
-    paypal_charge_constant = db.Column(db.Numeric(10,2), default=D('0.0')) #0.3
+    paypal_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #3%
+    paypal_charge_constant = db.Column(db.Numeric(10,2), default=D('0.0')) #$0.3
+    paypal_charge_constant_currency = db.Column(db.String(3), default='USD')
 
-    foreign_exchange_rate = db.Column(db.Numeric(10,2), default=D('0.0')) #101.4
-    foreign_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #1.5
-    foreign_charge_constant = db.Column(db.Numeric(10,2), default=D('0.0')) #75
+    local_currency = db.Column(db.String(3), default='KES')
+    foreign_currency = db.Column(db.String(3), default='USD')
+    foreign_exchange_rate = db.Column(db.Numeric(10,2), default=D('0.0')) #101.4 KES
 
-    service_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #4.5
-    service_charge_max = db.Column(db.Numeric(10,2), default=D('0.0')) #250
-    mobile_money_charge = db.Column(db.Numeric(10,2), default=D('0.0')) #33
+    foreign_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #0%
+    foreign_charge_constant = db.Column(db.Numeric(10,2), default=D('0.0')) #3 KES
+    foreign_charge_constant_currency = db.Column(db.String(3), default='KES')
+
+    service_charge_percentage = db.Column(db.Numeric(10,2), default=D('0.0')) #4.5%
+    service_charge_constant = db.Column(db.Numeric(5, 2), default=D('0.0')) #$1
+    service_charge_constant_currency = db.Column(db.String(3), default='USD')
+
+    service_charge_max = db.Column(db.Numeric(10,2), default=D('0.0')) #10000 KES
+    service_charge_max_currency = db.Column(db.String(3), default='USD')
+
+    mobile_money_charge = db.Column(db.Numeric(10,2), default=D('0.0')) #66 KES
+    mobile_money_charge_currency = db.Column(db.String(3), default='KES')
 
     date_created = db.Column(db.DateTime(), nullable=False)
 
-    def _get_percentage_service_charge(self, amount): 
-        return (amount * (self.service_charge_percentage / D('100')))
+    def _do_foreign_exchange(self, amount, from_currency='USD', 
+        to_currency='KES', exchange_rate=None):
+        acceptable_currencies = (self.foreign_currency, self.local_currency)
 
-    def _do_foreign_exchange(self, foreign_amount, from_currency='USD', to_currency='KES'):
-        return (self.foreign_exchange_rate * foreign_amount)
+        if from_currency not in acceptable_currencies:
+            return
+        elif to_currency not in acceptable_currencies:
+            return
+        elif from_currency == self.local_currency and to_currency == self.foreign_currency:
+            return amount
+        elif from_currency == self.foreign_currency and to_currency == self.local_currency:
+            return ((exchange_rate or self.foreign_exchange_rate) * amount)
+        else:
+            return D('0.0')
 
-    def _deduct_paypal_percentage(self, foreign_amount):
-        difference = (D('100') - self.paypal_charge_percentage) / D('100') 
-        return round_up((foreign_amount * difference) - self.paypal_charge_constant)
+    def _get_paypal_percentage(self, amount, currency=None):
+        percentage = self.paypal_charge_percentage / D('100')
+        percentage_charge = (amount * percentage)
+
+        if currency is None:
+            currency = self.foreign_currency #usd presumably
+
+        return percentage_charge + self.paypal_charge_constant
     
-    def _deduct_foreign_charge(self, subtotal):
-        difference = (D('100') - self.foreign_charge_percentage) / D('100') 
-        return round_up((subtotal * difference) - self.foreign_charge_constant)
+    def _get_foreign_charge(self, subtotal, currency=None):
+        if currency is None:
+            currency = self.foreign_charge_constant_currency
+        
+        percentage_charge = D('0')
+        if self.foreign_charge_percentage:
+            percentage = self.foreign_charge_percentage / D('100')
+            percentage_charge = (subtotal * percentage)
 
-    def _deduct_service_charge(self, subtotal):
-        service_charge = self._get_percentage_service_charge(subtotal)
-        minimum_deduction = min(service_charge, self.service_charge_max)
+        constant_charge = D('0.0')
+        if currency == self.foreign_charge_constant_currency:
+            constant_charge = self.foreign_charge_constant
+        elif not currency == self.foreign_charge_constant_currency:
+            constant_charge = self._do_foreign_exchange(
+                self.foreign_charge_constant, 
+                from_currency=self.foreign_charge_constant_currency, 
+                to_currency=self.local_currency)
 
-        return round_up(subtotal - minimum_deduction)
+        return (percentage_charge + constant_charge)
 
-    def _deduct_mobile_money_charge(self, subtotal): 
-        return round_up(subtotal - self.mobile_money_charge)
+    def _get_service_charge(self, subtotal, currency=None):
+        def get_percentage_service_charge(amount): 
+            return (amount * (self.service_charge_percentage / D('100')))
 
-    def get_exchange_amount(self, amount, from_currency='USD', to_currency='KES'):
-        foreign_total = self._deduct_paypal_percentage(amount)
+        percentage_service_charge = get_percentage_service_charge(subtotal)
 
-        subtotal = self._do_foreign_exchange(foreign_total, 
-            from_currency=from_currency, to_currency=to_currency)
-        subtotal = self._deduct_foreign_charge(subtotal)
-        subtotal = self._deduct_service_charge(subtotal)
-        total = self._deduct_mobile_money_charge(subtotal)
+        if currency is None:
+            currency = self.service_charge_constant_currency
+
+        constant_charge = D('0.0')
+        if currency == self.service_charge_constant_currency:
+            constant_charge = self.service_charge_constant
+        else:
+            constant_charge = self._do_foreign_exchange(
+                self.service_charge_constant, from_currency=currency, 
+                to_currency=self.service_charge_constant_currency)
+
+        return min(
+            percentage_service_charge + (constant_charge or D('0.0')), 
+            self._do_foreign_exchange(self.service_charge_max,
+                from_currency=self.service_charge_max_currency, to_currency=currency))
+
+    def _get_mobile_money_charge(self, currency=None):
+        if currency is None:
+            currency = self.mobile_money_charge_currency
+        
+        if currency == self.mobile_money_charge_currency:
+            return self.mobile_money_charge
+        else:
+            return self._do_foreign_exchange(self.mobile_money_charge,
+                from_currency=currency, to_currency=self.mobile_money_charge_currency)
+
+    def get_exchange_amount(self, amount, from_currency=None, to_currency=None):
+        if from_currency is None or to_currency is None:
+            from_currency = self.foreign_currency
+            to_currency = self.local_currency
+        
+        #--------------- less paypal charge
+        paypal_charge = 0
+        if self.paypal_charge_percentage and self.paypal_charge_constant:
+            paypal_charge = self._get_paypal_percentage(amount, currency=from_currency)
+        
+        less_paypal_charge = amount - paypal_charge
+
+        #--------------- less service charge
+        service_charge = self._get_service_charge(less_paypal_charge, currency=from_currency)
+        less_service_charge = less_paypal_charge - service_charge
+
+        #--------------- less foreign exchange charge
+        foreign_exchange_rate = self.foreign_exchange_rate
+        foreign_exchange_rate_currency = self.local_currency
+
+        foreign_charge = self._get_foreign_charge(
+            foreign_exchange_rate, currency=foreign_exchange_rate_currency)
+        exchange_rate = foreign_exchange_rate - foreign_charge
+        
+        #--------------- perform foreign exchange
+        foreign_exchange = self._do_foreign_exchange(less_service_charge, 
+            exchange_rate=exchange_rate)
+            
+        mobile_money_charge = self._get_mobile_money_charge(to_currency)
+        total = foreign_exchange - mobile_money_charge
 
         if total > 0:
-            return total
+            return dict(
+                equivalent_amount=less_paypal_charge,
+                total=round_down(total))
 
-        return 0
+        return dict(
+            equivalent_amount=0, total=0)
 
     def get_effective_rate(amount):
         total_charge = self.get_total_charge(amount)
 
-# --------------------------------------------
-# PP: 3% and $.3 = $96.7
-# FX: 101.4
-# --------------------------------------------
-# SUBTOTAL: 9,805.38
-# --------------------------------------------
-
-# FX: 1.5% + 75 KES 
-# --------------------------------------------
-# SUBTOTAL: (9,805.38 * 98.5%) - 75 = 9,583.29
-# --------------------------------------------
-
-# US: min((4.5% + 33), (250 + 33)) KES
-# --------------------------------------------
-# SUBTOTAL: (9,583.29 - 250) - 33 = 9,300.29
-# --------------------------------------------
-
-# --------------------------------------------
-# EFFECTIVE RATE: KES. 93
-# --------------------------------------------
